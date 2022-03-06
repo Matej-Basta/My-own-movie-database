9ca52523a137a18bb928e7103d4757e5462da73b
matej <matej.basta@email.cz>
2022-03-06 12:20:40 +0100

    initial setup

---
+++ database/migrations/2022_03_06_110443_create_movies_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("movie_type_id");
+            $table->foreignId("movie_status_id");
+            $table->string("name");
+            $table->integer("length");
+            $table->year("year");
+            $table->year("start_year");
+            $table->year("end_year");
+            $table->float("rating");
+            $table->integer("votes_nr");
+            $table->integer("metascore");
+            $table->foreignId("certification_id");
+            $table->unsignedBigInteger("budget");
+            $table->string("budget_currency");
+            $table->string("color_code");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+};
